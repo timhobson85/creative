683133f3bf6fecf4e8c9a83c70099373e1347230
Tim Hobson <claehobson@gmail.com>
2020-04-27 01:30:48 +1000

    added angles and colors

--- P5system/sketch.js
+++ P5system/sketch.js
@@ -7,23 +7,35 @@
 
 const CRYSTAL_SIZE = 500;
 const SIDES = 6;
+let PALETTE = []
 
 
 function setup() {
-  createCanvas(500, 500, SVG); // height, width, and export type
+  createCanvas(550, 550, SVG); // height, width, and export type
+  PALETTE = [
+    color(255, 52, 154), //pink
+    color(4, 0, 152) //blue
+  ]
   noLoop(); // only one render
   angleMode(DEGREES); // makes the correct rotate value
   rectMode(CENTER); // sets co-ordinates from centre, not from top left.
 }
 
 function draw() {
-  background('teal');
-  fill(0);
+  testLines();
+}
+
+function testLines() {
+  noFill(0);
   push(); // push pop only applies this transform/rotate to what's inside. then goes back
     translate(width/2, height/2); // this moves the x,y 0,0 to the centre(or whereever you set it)
-    rotate(45)
-    rect(0, 0, 25, 25);
+    stroke(PALETTE[0])
+    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
+    stroke(PALETTE[1])
+    const angle = 360 / SIDES
+    for (let i = 0; i < SIDES; i++) {
+      line(0, 0, 0, CRYSTAL_SIZE/2)
+      rotate( angle )
+    }
   pop();
-  fill('red')
-  rect(0, 0, 25, 25);
 }
